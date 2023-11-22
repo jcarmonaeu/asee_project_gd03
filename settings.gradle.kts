a4bb0d5b9c6ee9473c1ea4c82f0d99efd461165c
Jorge <jorge.carleo00@gmail.com>
2023-11-22 12:24:50 +0100

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "asee_project_gd03"
+include(":app")
+ 
